e135b668dc7afa88d203a6fd4a2d6d88828ed7f8
takiindev <takin2k5@gmail.com>
2024-10-16 00:07:02 +0700

    upload

--- public/share/js/header.js
+++ public/share/js/header.js
@@ -22,10 +22,9 @@ document.querySelector('header').innerHTML += `
                         <div class="searchbox__button">
                             <button class="header__login" popovertarget = "sign-in__form">
                                 <img class="login__img1" src="../../assets/img/header/Property 1=Login_Default.png" alt="Login Button">
-                                <img class="login__img2" src="../../assets/img/header/Property 1=Login_hover.png" alt="Login Button">
                             </button>
                         </div>
-                        <input class="searchbox__input" type="text" placeholder="Đăng nhập">
+                        <input class="searchbox__input" type="button" value="Đăng nhập">
                     </div>
                 </div>
             </div>
@@ -37,7 +36,14 @@ document.querySelector('header').innerHTML += `
         </style>
   `
 
-
+// Change value on hover
+const searchboxInput = document.querySelector('.searchbox__input');
+searchboxInput.addEventListener('mouseover', () => {
+    searchboxInput.value = 'Đăng nhập ngay';
+});
+searchboxInput.addEventListener('mouseout', () => {
+    searchboxInput.value = 'Đăng nhập';
+});
 
 
 // load header.css
@@ -45,81 +51,3 @@ document.querySelector('head').innerHTML += `
         <link rel="stylesheet" href="../../share/css/basic.css">
         <link rel="stylesheet" href="../../share/css/header.css">
         `
-
-// hover searchbox
-let focus = false;
-const heaeder__right = document.querySelector(".header--right")
-const searchbox__input = document.querySelector(".searchbox__input")
-const header__searchbox = document.querySelector(".header__searchbox")
-searchbox__input.addEventListener('mouseenter', function () {
-    searchbox__input.style.background = "linear-gradient(to right, #FE7F00 10%,#C4000C 90%)"
-    header__searchbox.style.width = "246px"
-    searchbox__input.style.width = "100%"
-    // searchbox__input.placeholder = "_______________________"
-    searchbox__input.placeholder = "Từ khoá tìm kiếm"
-    searchbox__input.style.paddingLeft = "35px";
-    searchbox__input.style.paddingRight = "13px";
-    searchbox__input.style.right = "0px"
-    searchbox__input.style.color = "white"
-    searchbox__input.style.left = "0px";
-});
-searchbox__input.addEventListener('mouseleave', function () {
-    let value = document.querySelector(".searchbox__input").value
-    if (value == "" && focus == false) {
-        header__searchbox.style.width = "103px"
-        searchbox__input.style.width = "100%"
-        searchbox__input.style.paddingLeft = "33px";
-        searchbox__input.style.paddingRight = "0";
-        setTimeout(() => {
-            searchbox__input.placeholder = "Tìm kiếm"
-        }, 100);
-        searchbox__input.style.right = "initial"
-        setTimeout(() => {
-            console.log("run")
-            document.querySelector(".searchbox__input").style.background = "linear-gradient(to right, #FFFFFF 10%,#FFFFFF 80%)"
-            searchbox__input.style.setProperty('--placeholder-color', 'black');
-
-        }, 1);
-    }
-    if (value == "" && focus == true) {
-        searchbox__input.style.setProperty('--placeholder-color', 'white');
-    }
-});
-
-
-// focus searchbox
-setTimeout(() => {
-    document.querySelector(".searchbox__input").style.transition = "all 0.2s linear";
-}, 100);
-searchbox__input.addEventListener('focus', function () {
-    focus = true;
-    searchbox__input.style.background = "linear-gradient(to right, #FE7F00 10%,#C4000C 90%)"
-    header__searchbox.style.width = "246px"
-    searchbox__input.style.width = "100%"
-    searchbox__input.placeholder = "Từ khoá tìm kiếm"
-    searchbox__input.style.paddingLeft = "35px";
-    searchbox__input.style.paddingRight = "13px";
-    searchbox__input.style.right = "0px"
-    searchbox__input.style.color = "white"
-    searchbox__input.style.left = "0px";
-});
-searchbox__input.addEventListener('blur', function () {
-    focus = false;
-    let value = document.querySelector(".searchbox__input").value
-    if (value == "") {
-        header__searchbox.style.width = "103px"
-        searchbox__input.style.width = "100%"
-        searchbox__input.style.paddingLeft = "33px";
-        searchbox__input.style.paddingRight = "0";
-        setTimeout(() => {
-            searchbox__input.placeholder = "Tìm kiếm"
-        }, 100);
-        searchbox__input.style.right = "initial"
-        setTimeout(() => {
-            console.log("run")
-            document.querySelector(".searchbox__input").style.background = "linear-gradient(to right, #FFFFFF 10%,#FFFFFF 80%)"
-            searchbox__input.style.setProperty('--placeholder-color', 'black');
-        }, 1);
-    }
-});
-
